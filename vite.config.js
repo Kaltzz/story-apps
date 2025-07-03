c7de79f44bf96d07553a2f864237bb907e7a0888
Kaltzz <ahmadrafly682@gmail.com>
2025-07-03 23:39:22 +0800

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/story-app-final/', // Sesuaikan dengan nama repo Anda
+  base: '/story-apps/', // Sesuaikan dengan nama repo Anda
 
   plugins: [
     VitePWA({
